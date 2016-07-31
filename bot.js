24069e93b949e7b30846ba9932cc68f1eeb3e586
d_ojima <daiking.ca2@gmail.com>
2016-07-31 18:00:56 +0900

    commit

--- bot.js
+++ bot.js
@@ -42,5 +42,5 @@ controller.hears('coke', ['direct_message', 'mention', 'ambient'], function (bot
 });
 
 controller.hears('[0-9]{10}', ['direct_message', 'mention', 'ambient'], function (bot, message) {
-    bot.reply(message, JSON.stringify(message);
+    bot.reply(message, JSON.stringify(message));
 });
