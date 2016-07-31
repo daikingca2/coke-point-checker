c29418b5c3eeb84a10177f1251e0eae3d7c2a79d
d_ojima <daiking.ca2@gmail.com>
2016-07-31 13:21:17 +0900

    first commit

---
+++ bot.js
@@ -0,0 +1,24 @@
+// use botkit
+var botkit = require('botkit');
+// var os = require('os');
+
+// controller
+var controller = botkit.slackbot({
+    debug: false
+});
+
+// check api token
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+// start rtm
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+// say hi (test)
+controller.hears('hi', ['direct_message', 'direct_message', 'mention'], function (bot, message) {
+    bot.reply(message, 'hi');
+});
